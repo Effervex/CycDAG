a3d2b5e3f892b9d19587f2843939909dc097c595
Sam Sarjant <sam.sarjant@gmail.com>
2013-12-18 16:23:27 +1300

    Modified copyright notice to GPL V3.

--- src/graph/module/TransitiveIntervalSchemaModule.java
+++ src/graph/module/TransitiveIntervalSchemaModule.java
@@ -1,5 +1,12 @@
 /*******************************************************************************
- * Copyright (C) 2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013 University of Waikato, Hamilton, New Zealand.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Sam Sarjant - initial API and implementation
  ******************************************************************************/
 package graph.module;
 
@@ -57,6 +64,8 @@ public class TransitiveIntervalSchemaModule extends
 	 * @param topologicalList
 	 *            The topologically sorted list of nodes.
 	 * @param predecessorTree
+	 *            If this tree is building a predecessor tree (alt. ancestor
+	 *            tree).
 	 * @return The spanning tree for the nodes.
 	 */
 	private Map<DAGNode, TreeNode> buildSpanningTree(
