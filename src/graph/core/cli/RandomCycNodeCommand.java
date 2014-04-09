f3ecaf5ba40a2c0531b271c31d1364ea3e10e73c
Sam Sarjant <sam.sarjant@gmail.com>
2014-04-09 17:01:28 +1200

    Changes to SubDAG extraction to hopefully make it slimmer and faster.

--- src/graph/core/cli/RandomCycNodeCommand.java
+++ src/graph/core/cli/RandomCycNodeCommand.java
@@ -10,8 +10,13 @@
  ******************************************************************************/
 package graph.core.cli;
 
+import java.util.Collection;
+import java.util.Random;
+
 import graph.core.CycDAG;
+import graph.core.DAGNode;
 import graph.core.Identifiable;
+import graph.module.SubDAGExtractorModule;
 import core.Command;
 
 public class RandomCycNodeCommand extends Command {
@@ -32,8 +37,23 @@ public class RandomCycNodeCommand extends Command {
 		boolean allowFunctions = true;
 		if (data.equals("F"))
 			allowFunctions = false;
-		Identifiable obj = ((CycDAG) dagHandler.getDAG())
-				.getRandomNode(allowFunctions);
+
+		String filter = dagHandler.get(DAGPortHandler.SUBDAG_FILTERING);
+		Identifiable obj = null;
+		if (filter == null)
+			obj = ((CycDAG) dagHandler.getDAG()).getRandomNode(allowFunctions);
+		else {
+			SubDAGExtractorModule subDAGModule = (SubDAGExtractorModule) dagHandler
+					.getDAG().getModule(SubDAGExtractorModule.class);
+			Collection<DAGNode> tagged = subDAGModule.getTagged(filter);
+			if (tagged != null && !tagged.isEmpty()) {
+				DAGNode[] nodeArray = tagged
+						.toArray(new DAGNode[tagged.size()]);
+				Random rand = new Random();
+				obj = nodeArray[rand.nextInt(nodeArray.length)];
+			}
+		}
+
 		if (obj == null)
 			print("-1|No nodes exist.\n");
 		else
--- src/graph/module/SubCycDAGExtractorModule.java
+++ src/graph/module/SubCycDAGExtractorModule.java
@@ -12,10 +12,8 @@ package graph.module;
 
 import graph.core.CommonConcepts;
 import graph.core.CycDAG;
-import graph.core.DAGEdge;
 import graph.core.DAGNode;
 import graph.core.DirectedAcyclicGraph;
-import graph.core.Edge;
 import graph.core.Node;
 import graph.core.OntologyFunction;
 import graph.inference.CommonQuery;
@@ -31,53 +29,38 @@ public class SubCycDAGExtractorModule extends SubDAGExtractorModule {
 	private QueryModule querier_;
 
 	@Override
-	public void followEdges(Collection<DAGNode> nodes, int distance,
+	public void findLinks(Collection<DAGNode> nodes,
+			Collection<DAGNode> existingNodes, int distance,
 			RelatedEdgeModule relatedEdgeModule) {
-		// Similar process, but define the ontology as connected to an upper
-		// node (Thing)
 		Collection<Node> linkedNodes = new HashSet<>();
 		boolean allowIndividuals = true;
-
-		// Retrieve ALL upwards and downwards nodes & links from tagged nodes
 		for (Node n : nodes) {
-			if (allowIndividuals)
-				linkedNodes.addAll(CommonQuery.INSTANCES.runQuery(dag_, n));
-			linkedNodes.addAll(CommonQuery.SPECS.runQuery(dag_, n));
-			linkedNodes.addAll(CommonQuery.SPECPREDS.runQuery(dag_, n));
-		}
+			// Follow the nodes all the way up
+			linkedNodes.addAll(CommonQuery.ALLISA.runQuery(dag_, n));
+			linkedNodes.addAll(CommonQuery.ALLGENLS.runQuery(dag_, n));
+			linkedNodes.addAll(CommonQuery.GENLPREDS.runQuery(dag_, n));
 
-		// Follow non-taxonomic nodes as normal
-		Collection<DAGNode> completed = new HashSet<>();
-		Collection<DAGNode> currentLevel = nodes;
-		for (int d = distance; d > 0; d--) {
-			Collection<DAGNode> nextLevel = new HashSet<>();
-			// For every node on this level
-			for (DAGNode n : currentLevel) {
-				// Follow non taxonomic edges
-				Collection<Edge> relatedEdges = relatedEdgeModule.execute(n);
-				for (Edge e : relatedEdges) {
-					// Non-taxonomic only!
-					if (!e.equals(CommonConcepts.ISA.getNode(dag_))
-							&& !e.equals(CommonConcepts.GENLS.getNode(dag_))
-							&& !e.equals(CommonConcepts.GENLPREDS.getNode(dag_))) {
-						// Grab every non-predicate argument
-						Node[] args = e.getNodes();
-						for (int i = 1; i < args.length; i++) {
-							// Add to next level if not already looked at
-							if (args[i] instanceof DAGNode
-									&& !completed.contains(args[i]))
-								nextLevel.add((DAGNode) args[i]);
-						}
-					}
-				}
+			// Follow the nodes all the way down (if distance is positive)
+			if (distance > 0) {
+				if (allowIndividuals)
+					linkedNodes.addAll(CommonQuery.INSTANCES.runQuery(dag_, n));
+				linkedNodes.addAll(CommonQuery.SPECS.runQuery(dag_, n));
+				linkedNodes.addAll(CommonQuery.SPECPREDS.runQuery(dag_, n));
 			}
-			currentLevel = nextLevel;
-			// Add all the next level nodes
-			linkedNodes.addAll(nextLevel);
 		}
 
 		for (Node n : linkedNodes)
+			processNode(n, nodes, existingNodes);
+	}
+
+	private void processNode(Node n, Collection<DAGNode> nodes,
+			Collection<DAGNode> existingNodes) {
+		if (n instanceof DAGNode && !nodes.contains(n)
+				&& !existingNodes.contains(n)) {
 			nodes.add((DAGNode) n);
+			processFunction(n, nodes, existingNodes);
+			processPredicate(n, nodes, existingNodes);
+		}
 	}
 
 	@Override
@@ -89,45 +72,24 @@ public class SubCycDAGExtractorModule extends SubDAGExtractorModule {
 	}
 
 	@Override
-	protected void preAssertionProcessing(Collection<DAGNode> newlyAddedNodes,
-			Collection<DAGNode> processedNodes, Collection<DAGEdge> edges) {
-		Collection<DAGNode> parentNodes = new HashSet<>();
-		for (Node node : newlyAddedNodes)
-			processNode(node, parentNodes, processedNodes);
-		newlyAddedNodes.addAll(parentNodes);
-
-		super.preAssertionProcessing(newlyAddedNodes, processedNodes, edges);
+	public boolean growSubDAG(String tag, int distance) {
+		querier_ = (QueryModule) dag_.getModule(QueryModule.class);
+		return super.growSubDAG(tag, distance);
 	}
 
-	private void processNode(Node node, Collection<DAGNode> parentNodes,
-			Collection<DAGNode> processedNodes) {
-		// Follow node upwards
-		if (!(node instanceof DAGNode))
-			return;
-		Collection<Node> parents = CommonQuery.ALLISA.runQuery(dag_, node);
-		parents.addAll(CommonQuery.ALLGENLS.runQuery(dag_, node));
-		parents.addAll(CommonQuery.GENLPREDS.runQuery(dag_, node));
-		for (Node parent : parents) {
-			if (parent instanceof DAGNode) {
-				if (!processedNodes.contains(parent)
-						&& parentNodes.add((DAGNode) parent)) {
-					processFunction(parent, parentNodes, processedNodes);
-					processPredicate(parent, parentNodes, processedNodes);
-				}
-			}
-		}
-
-		if (!processedNodes.contains(node) && parentNodes.add((DAGNode) node)) {
-			// Add function components
-			processFunction(node, parentNodes, processedNodes);
-
-			// Add arg constraints for predicates and functions
-			processPredicate(node, parentNodes, processedNodes);
-		}
-	}
-
-	private void processPredicate(Node node, Collection<DAGNode> parentNodes,
-			Collection<DAGNode> processedNodes) {
+	/**
+	 * Processes a predicate node, adding its semantic arg constraints to the
+	 * subDAG.
+	 * 
+	 * @param node
+	 *            The node to process.
+	 * @param nodes
+	 *            The set of nodes to add to.
+	 * @param existingNodes
+	 *            Existing nodes already integrated.
+	 */
+	private void processPredicate(Node node, Collection<DAGNode> nodes,
+			Collection<DAGNode> existingNodes) {
 		VariableNode y = new VariableNode("?Y");
 		VariableNode x = new VariableNode("?X");
 		Collection<Substitution> subs = querier_.execute(
@@ -140,18 +102,27 @@ public class SubCycDAGExtractorModule extends SubDAGExtractorModule {
 				node, y));
 		for (Substitution sub : subs) {
 			Node constraint = sub.getSubstitution(y);
-			if (constraint instanceof DAGNode
-					&& parentNodes.add((DAGNode) constraint))
-				processNode(constraint, parentNodes, processedNodes);
+			if (constraint instanceof DAGNode)
+				processNode(constraint, nodes, existingNodes);
 		}
 	}
 
-	private void processFunction(Node node, Collection<DAGNode> parentNodes,
-			Collection<DAGNode> processedNodes) {
+	/**
+	 * Processes a NART/NAT, adding its components to the subDAG.
+	 * 
+	 * @param node
+	 *            The node to process.
+	 * @param nodes
+	 *            The set of nodes to add to.
+	 * @param existingNodes
+	 *            Existing nodes already integrated.
+	 */
+	private void processFunction(Node node, Collection<DAGNode> nodes,
+			Collection<DAGNode> existingNodes) {
 		if (node instanceof OntologyFunction) {
 			Node[] funcNodes = ((OntologyFunction) node).getNodes();
 			for (Node n : funcNodes)
-				processNode(n, parentNodes, processedNodes);
+				processNode(n, nodes, existingNodes);
 		}
 	}
 }
